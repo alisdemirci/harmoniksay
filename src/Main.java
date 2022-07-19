fe9e9c5c24a4e5b16946b579d0bfb4c09b83a6ee
alisdemirci <106967940+alisdemirci@users.noreply.github.com>
2022-07-19 23:49:30 +0300

    abcd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import java.util.Scanner ;
+class s {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner inp = new Scanner(System.in);
+        int n, total = 0 ;
+
+        do {
+            System.out.print("Enter a number : ");
+            n = inp.nextInt();
+            if (n % 4 == 0){
+                total += n ;
+            }
+
+        } while (n % 2 == 0);
+        System.out.println("The total is : " + total);
+
+
     }
 }
